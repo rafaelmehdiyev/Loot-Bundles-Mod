e2a0159e440c30e0991fa7dee20c00b857837081
Rafael Mehdiyev <rafaelmehdiyev0@gmail.com>
2024-11-10 12:49:06 +0400

    Initial commit

---
+++ src/main/java/net/rafael/lootbundle/RafaelsLootBundlesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rafael.lootbundle;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RafaelsLootBundlesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
