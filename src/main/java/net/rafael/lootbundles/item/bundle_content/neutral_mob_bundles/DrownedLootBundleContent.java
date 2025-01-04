76ce6d982503871e465a3cb92208bdea8c20ec68
Rafael Mehdiyev <rafaelmehdiyev0@gmail.com>
2025-01-04 11:10:18 +0400

    Added Drowned Bundle

--- src/main/java/net/rafael/lootbundles/item/bundle_content/neutral_mob_bundles/DrownedLootBundleContent.java
+++ src/main/java/net/rafael/lootbundles/item/bundle_content/neutral_mob_bundles/DrownedLootBundleContent.java
@@ -6,13 +6,14 @@ import net.minecraft.item.Items;
 import java.util.HashMap;
 import java.util.Map;
 
-public class EndermanLootBundleContent {
+public class DrownedLootBundleContent {
     public static Map<Item, Integer> getItems() {
         Map<Item, Integer> items = new HashMap<>();
-        items.put(Items.ENDER_PEARL, 8);
-        items.put(Items.ENDER_EYE, 4);
-
+        items.put(Items.ROTTEN_FLESH, 7);
+        items.put(Items.COPPER_INGOT, 3);
+        items.put(Items.NAUTILUS_SHELL, 1);
+        items.put(Items.TRIDENT, 1);
+        items.put(Items.FISHING_ROD,2);
         return items;
     }
 }
-
